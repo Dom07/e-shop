f68fcd9c1c5389bce39d31afe51a9d70dc35a010
Dominic <dd429@njit.edu>
2020-03-28 20:44:16 -0400

    Updated Navbar to load sub categories

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,39 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, Nav, Form, Button, FormControl, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import Axios from 'axios';
 
-const navItems = ["Electronics", "Health Care", "Clothing", "Entertainment"]
+class NavBar extends Component {
+    state = {
+        Electronics:[],
+        HealthCare: [],
+        Clothing: [],
+        Entertainment: []
+    }
 
-const showNavItems = () => navItems.map(item => <NavDropdown title={item} id="basic-nav-dropdown">
-    <NavDropdown.Item>Action</NavDropdown.Item>
-    <NavDropdown.Item>Another action</NavDropdown.Item>
-    <NavDropdown.Item>Something</NavDropdown.Item>
-    <NavDropdown.Item>Separated link</NavDropdown.Item>
-</NavDropdown>)
+    navItems = ["Electronics", "HealthCare", "Clothing", "Entertainment"]
 
-const NavBar = (props) => {
-    return (
-        <div>
-            <Navbar bg="primary" variant="dark">
-                <LinkContainer to="/">
-                    <Navbar.Brand>E-Shop</Navbar.Brand>
-                </LinkContainer>
-                <Nav className="mr-auto">
-                    <Form inline>
-                        <FormControl type="text" style={{ marginLeft: "50px" }} placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-light">Search</Button>
-                    </Form>
-                    <Nav style={{ marginLeft: "20px" }}>
-                        {showNavItems()}
+    loadNavSubItems = () => {
+        this.navItems.forEach(item => {
+            Axios.get("http://localhost:4000/api/subCategory/"+item,)
+                .then(response => {
+                    let oldState = {...this.state}
+                    oldState[item] = response.data.SUCCESS
+                    this.setState(oldState)
+                })
+                .catch(error => console.log(error))
+        })
+    }
+
+    renderSubCategories = (category) =>{
+        if(this.state[category].length > 0){
+            return this.state[category].map(item => <NavDropdown.Item>
+                {item.name}
+            </NavDropdown.Item>)
+        }else{
+            return <div>No Items</div>
+        }
+    } 
+
+    renderNavItems = () => this.navItems.map(item => 
+    <NavDropdown title={item} id="basic-nav-dropdown" key={item}>
+        {this.renderSubCategories(item)}
+    </NavDropdown>)
+
+    componentDidMount() {
+        this.loadNavSubItems();
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar bg="primary" variant="dark">
+                    <LinkContainer to="/">
+                        <Navbar.Brand>E-Shop</Navbar.Brand>
+                    </LinkContainer>
+                    <Nav className="mr-auto">
+                        <Form inline>
+                            <FormControl type="text" style={{ marginLeft: "50px" }} placeholder="Search" className="mr-sm-2" />
+                            <Button variant="outline-light">Search</Button>
+                        </Form>
+                        <Nav style={{ marginLeft: "20px" }}>
+                            {(this.state.Electronics.length > 0) ? this.renderNavItems() : "Loading..." }
+                        </Nav>
                     </Nav>
-                </Nav>
-                <Nav>
-                    <Nav.Link href="#cart"><i className="fas fa-shopping-cart" style={{ fontSize: "1.5rem" }} ></i></Nav.Link>
-                    <Nav.Link href="#home">Sign In</Nav.Link>
-                </Nav>
-            </Navbar>
-        </div>
-    )
+                    <Nav>
+                        <Nav.Link href="#cart"><i className="fas fa-shopping-cart" style={{ fontSize: "1.5rem" }} ></i></Nav.Link>
+                        <Nav.Link href="#home">Sign In</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </div>
+        )
+    }
 };
 
 export default NavBar;
--- src/components/ProductThumbNails/ProductThumbNail/ProductThumbNail.js
+++ src/components/ProductThumbNails/ProductThumbNail/ProductThumbNail.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const ProductThumbNail = (props) => {
   return (
     <Card style={{ width: '15rem' }}>
-      <Card.Img variant="top" src={props.image} style={{height: "200px"}}/>
+      <Card.Img variant="top" src={props.image} style={{height: "200px", objectFit: "contain", padding: "10px"}}/>
       <Card.Body>
         <Card.Title style={{ margin: "0px" }}>{props.name}</Card.Title>
         <Card.Text style={{ margin: "0px" }}>
@@ -13,8 +13,8 @@ const ProductThumbNail = (props) => {
           <span>5</span>
         </Card.Text>
         <Card.Text style={{ margin: "0px" }}>
-          <span>Price:</span>
-          <span>{props.price}</span>
+          <span>Price: </span>
+          <span>${props.price}</span>
         </Card.Text>
         <Link to={"/product/"+props.id}>
           <p style={{ color: "#007bff", cursor: "pointer" }}>Show Details</p>
